e1a44c5ed385622033df74c8113e14f9f6c4c3b7
steffytw <steffythankamwilson@cs.ajce.in>
2017-10-24 17:44:08 +0530

    first commit

--- iris1.py
+++ iris1.py
@@ -1,3 +1,37 @@
+from scipy.spatial import distance
+
+def eucli(a,b):
+   return distance.euclidean(a,b)
+
+class myKNN():
+
+#define method in a classifier
+
+  def fit(self,X_train,Y_train):
+    self.X_train = X_train
+    self.Y_train = Y_train
+
+#predict method of classifier
+
+  def predict(self,X_train):
+    predictions = []
+    for row in X_test:
+      labels = self.closest(row)
+      predictions.append(labels)
+    return predictions
+
+#closest distance
+
+  def closest(self,row):
+    best_dist = eucli(row,self.X_train[0])
+    best_index = 0
+    for i in range(1, len(self.X_train)):
+      dist = eucli(row,self.X_train[i])
+      if dist < best_dist:
+       best_dist = dist
+       best_index = i
+    return self.Y_train[best_index]
+
 from sklearn.neighbors  import KNeighborsClassifier
 from sklearn.datasets import load_iris
 
@@ -9,7 +43,7 @@ labels = iris.target
 from sklearn.cross_validation import train_test_split
 X_train,X_test,Y_train,Y_test = train_test_split(features,labels,test_size=.3)
 
-clf =KNeighborsClassifier()
+clf =myKNN()
 clf.fit(X_train,Y_train)
 
 p = clf.predict(X_test)
